cc1a6dca0ad09ddf10420afecc73a06160e9bae5
ElenaDergaus <104501033+ElenaDergaus@users.noreply.github.com>
2022-04-30 23:22:04 +0300

    Add files via upload

--- Constellation.m
+++ Constellation.m
@@ -1,26 +1,39 @@
 classdef Constellation < handle
+% Класс Constellation позволяет создать объект, описывающий спутниковую
+% группировку, на основе параметров, считанных из конфигурационного файла,
+% определить положения КА в первоначальный момент времени и расчитать
+% дальнейшие положения КА в заданные моменты времени.
+% Время задается в эпохах.
 
     properties
-        totalSatCount = 0;
-        groups = {};
-        state;
+        totalSatCount = 0; % Общее количество спутников текущей орбитальной конфигурации
+        groups = {}; % Массиы ячеек для хранящая параметров спутниковых группировок 
+        state; % Структура для хранения результатов расчета 
 
-        % РєРѕРЅСЃС‚Р°РЅС‚С‹
-        earthRadius = 6378135;           % Р­РєРІР°С‚РѕСЂРёР°Р»СЊРЅС‹Р№ СЂР°РґРёСѓСЃ Р—РµРјР»Рё [km]
-        earthGM = 3.986004415e+14;       % Р“СЂР°РІРёС‚Р°С†РёРѕРЅРЅС‹Р№ РїР°СЂР°РјРµС‚СЂ Р—РµРјР»Рё [m3/s2]
-        earthJ2 = 1.082626e-3;           % First zonal harmonic coefficient in the expansion of the Earth's gravity field
+        % константы
+        earthRadius = 6378135;           % Экваториальный радиус Земли [m]
+        earthGM = 3.986004415e+14;       % Гравитационный параметр Земли [m3/s2]
+        earthJ2 = 1.082626e-3;           % Коэффициент первой зональной гармоники в расширении гравитационного поля Земли
     end
 
     methods
 
         function this = Constellation(varargin)
+            % Коснструктор класса.
+            % varargin принимает единственный параметр - наименование
+            % спутниковой группироки, по которому осуществляется поиск
+            %в конфигурационном файле constellationsTest.json
             if isempty(varargin)
-                return
+                msg = 'Не указан обязательный параметр - наименование группировки.';
+                error(msg)
             end
             this.loadFromConfigFile(varargin{1});
         end
 
         function loadFromConfigFile(this, code)
+            % Метод, используемый конструктором класса для инициализации
+            % параметров спутниковой группировки из конфигурационного
+            % файла.
             fileName = 'constellationsTest.json';
             str = fileread(fileName);
             data = jsondecode(str);
@@ -34,19 +47,20 @@ classdef Constellation < handle
             end
 
             if isempty(dataThis)
-                disp('Р“СЂСѓРїРїРёСЂРѕРІРєР° РЅРµ РЅР°Р№РґРµРЅР° РІ С„Р°Р№Р»Рµ');
+                disp('Группировка не найдена в файле');
                 return
             end
 
             for i = 1:size(dataThis.Walkers, 1)
-                group.inclination = deg2rad(dataThis.Walkers(i, 1));
-                group.satsPerPlane = dataThis.Walkers(i, 2);
-                group.planeCount = dataThis.Walkers(i, 3);
-                group.f = dataThis.Walkers(i, 4);
-                group.altitude = dataThis.Walkers(i, 5);
-                group.maxRaan = deg2rad(dataThis.Walkers(i, 6));
-                group.startRaan = deg2rad(dataThis.Walkers(i, 7));
-                group.totalSatCount = group.satsPerPlane * group.planeCount;
+                group.inclination   = deg2rad(dataThis.Walkers(i, 1)); % Наклонение орбит
+                group.satsPerPlane  = dataThis.Walkers(i, 2); % Количество спутников в плоскости
+                group.planeCount    = dataThis.Walkers(i, 3); % Количество плоскостей в группировке
+                group.f             = dataThis.Walkers(i, 4);
+                group.altitude      = dataThis.Walkers(i, 5); % Высота орбиты
+                group.maxRaan       = deg2rad(dataThis.Walkers(i, 6)); % Максимальное значение градуса дуги окружности,
+                                                                       % вдоль которой распределены плоскости орбит 
+                group.startRaan     = deg2rad(dataThis.Walkers(i, 7)); % Начальное значение 
+                group.totalSatCount = group.satsPerPlane * group.planeCount; % Общее количество спутников в группировке
 
                 this.groups{length(this.groups) + 1} = group;                
                 this.totalSatCount = this.totalSatCount + group.totalSatCount;
@@ -54,6 +68,7 @@ classdef Constellation < handle
         end
 
         function getInitialState(this)
+            % Функция, формирующая структуру элементов орбит КА
             this.state.elements = zeros(this.totalSatCount, 6);
             shift = 1;
 
@@ -67,25 +82,37 @@ classdef Constellation < handle
         end
 
         function elements = getInitialElements(this, group)
-            raans = linspace(group.startRaan, group.startRaan + group.maxRaan, group.planeCount + 1);
-            raans = mod(raans(1:end-1), 2 * pi);
+            % Функция вычисляет элементы орбиты для всех КА в начальный
+            % момент вемени
+            
+            % Вычисление положения орбитальных плоскостей
+            raanArray = linspace(group.startRaan, group.startRaan + group.maxRaan, group.planeCount + 1);
+            raanArray = mod(raanArray(1:end-1), 2 * pi);
 
             elements = zeros(group.totalSatCount, 6);
             idx = 1;
-            raanIDX = 0;
-            for raan = raans
+            raanIdx = 0;
+            
+            sma = this.earthRadius + group.altitude * 1000; %  *1000 - перевод высоты орбиты из км в м
+            
+            for raan = raanArray
                 for i = 0:group.satsPerPlane-1
-                    sma = this.earthRadius + group.altitude * 1000;
-                    aol = 2 * pi / group.satsPerPlane * i + 2 * pi / group.totalSatCount * group.f * raanIDX;
+                    
+                    % Угол дуги , на котором располагается каждый КА в отдельной орбитальной плоскости
+                    % с учетом смещения КА в следующей плоскости
+                    % относительно КА в предыдущей
+                    aol = 2 * pi / group.satsPerPlane * i + 2 * pi / group.totalSatCount * group.f * raanIdx;
 
                     elements(idx, :) = [sma, 0, 0, raan, group.inclination, aol];
                     idx = idx + 1;
                 end
-                raanIDX = raanIDX + 1;
+                raanIdx = raanIdx + 1;
             end
         end        
 
         function propagateJ2(this, epochs)
+            % Функция, расчитывающая положение всех КА в заданные моменты
+            % времени
             this.state.eci = zeros(this.totalSatCount, 3, length(epochs));
 
             inclination = this.state.elements(:, 5);
@@ -93,8 +120,10 @@ classdef Constellation < handle
             Omega0      = sqrt(this.earthGM ./ sma.^3);
             aol0        = this.state.elements(:, 6);
 
-            raanPrecessionRate = -1.5 * (this.earthJ2 * this.earthGM^(1/2) * this.earthRadius^2) ./ (sma.^(7/2)) .* cos(inclination);
-            draconicOmega      = sqrt(this.earthGM ./ sma.^3) .* (1 - 1.5 * this.earthJ2 .* (this.earthRadius ./ sma).^2) .* (1 - 4 .* cos(inclination).^2);
+            raanPrecessionRate = -1.5 * (this.earthJ2 * this.earthGM^(1/2) * this.earthRadius^2) ./...
+                                 (sma.^(7/2)) .* cos(inclination);
+            draconicOmega      = sqrt(this.earthGM ./ sma.^3) .* (1 - 1.5 * this.earthJ2 .* (this.earthRadius ./ sma).^2) .* ...
+                                 (1 - 4 .* cos(inclination).^2);
 
             for epochIdx = 1:length(epochs)
                 aol = aol0 + epochs(epochIdx) * draconicOmega;
