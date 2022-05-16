414bf65468f78f330636a4774b7e0eba77d0e87c
Gleb Belyy <dobrenok7319@gmail.com>
2022-05-16 20:58:42 +0300

    Update findVisibleSats.m

--- MATLAB/findVisibleSats.m
+++ MATLAB/findVisibleSats.m
@@ -1,5 +1,25 @@
 function coveredSats = findVisibleSats(constellation, fileName, epochList, epochIdx, elevAngleMinDeg)
-    
+
+    % ОПИСАНИЕ:
+    % Возвращает массив индексов спутников, видимых с конкретной шлюзовой станции.
+    % constellation - объект класса Constellation 
+    % fileName - имя файла с координатами расположения шлюзовых станций
+    % epochList - массив точек на оси времени, когда проводятся расчеты 
+    % epochIdx - индекс точки в массиве моментов расчета траектории
+    % спутников
+    % elevAngleMinDeg - минимальный угол места, при котором КА находится в
+    % зоне видимости шлюзовой станции [град.]
+    %
+    % ВХОДНЫЕ ДАННЫЕ:
+    % constellation - объект класса Constellation
+    % fileName - символьная строка
+    % epochList - одномерный массив скаляров
+    % epochIdx - целое положительное число, значение которого находится в диапазоне [1, length(epochList)]  
+    % elevAngleMinDeg - число от 0 до 90 
+    %
+    % ВЫХОДНЫЕ ЗНАЧЕНИЯ:
+    % coveredSats - двумерный массив ячеек, где каждая ячейка - массив индексов спутников, видимых с конкретной шлюзовой станции в данный момент времени.
+
     epoch = epochList(epochIdx);
     
     stationList = jsondecode(fileread(fileName));
