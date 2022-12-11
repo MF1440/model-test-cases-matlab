59a878941b169367ab81dd61053af41c0526f117
ggoyman <gordeygoyman@gmail.com>
2022-12-11 20:32:07 +0300

    Simple test checking refactoring doesn't affect code results

--- MATLAB/example.m
+++ MATLAB/example.m
@@ -20,4 +20,11 @@ epochIdx = ceil(length(epochs) * rand());
 disp(['Положение КА-' num2str(satIdx) ' на эпоху ' num2str(epochs(epochIdx)) ':']);
 disp(constellation.state.eci(satIdx, :, epochIdx));
 
+% Простой тест того, что рефакторинг кода не меняет результатов его работы
+testEpochIdx = 7;
+testSatIdx = 1630;
+testSatCoords = 1.0e+06 * [-2.141680404794564   0.983991843448561   6.695407797436292];
+testTreshold = 10^-15;
+assert(norm(testSatCoords-constellation.state.eci(testSatIdx, :, testEpochIdx))/norm(testSatCoords) <= testTreshold);
+
 toc
