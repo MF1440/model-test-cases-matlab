92a84bf167dbc7d565aa5c0636e1f285f24630c6
Ekaterina <katarinbgor@gmail.com>
2023-02-26 17:26:23 +0300

    Правки в именах переменных в Constellation.m

--- MATLAB/Constellation.m
+++ MATLAB/Constellation.m
@@ -2,10 +2,10 @@ classdef Constellation < handle
 
     properties
         totalSatCount = 0;
-        groups = {};
-        state;
-
-        % константы
+        groupList = {}; 
+        state = struct;
+        
+        % Константы
         earthRadius = 6378135;           % Экваториальный радиус Земли [m]
         earthGM = 3.986004415e+14;       % Гравитационный параметр Земли [m3/s2]
         earthJ2 = 1.082626e-3;           % Вторая зональная гармоника геопотенциала
@@ -14,6 +14,7 @@ classdef Constellation < handle
     methods
 
         function this = Constellation(varargin)
+            % varargin - ячейка с названием группировки
             if isempty(varargin)
                 return
             end
@@ -23,12 +24,12 @@ classdef Constellation < handle
         function loadFromConfigFile(this, code)
             fileName = 'constellationsTest.json';
             str = fileread(fileName);
-            data = jsondecode(str);
-            dataThis = [];
+            data = jsondecode(str);                      
+            dataThis = {};
 
-            for i = 1:length(data)
-                if strcmpi(data(i).name, code)
-                    dataThis = data(i);
+            for constellationIdx = 1:length(data)  % Ищем нужную группировку по имени
+                if strcmpi(data(constellationIdx).name, code)
+                    dataThis = data(constellationIdx);   
                     break
                 end
             end
@@ -38,17 +39,17 @@ classdef Constellation < handle
                 return
             end
 
-            for i = 1:size(dataThis.Walkers, 1)
-                group.inclination = deg2rad(dataThis.Walkers(i, 1));        % наклонение орбитальной плоскости
-                group.satsPerPlane = dataThis.Walkers(i, 2);				% число КА в каждой орбитальной плоскости группы
-                group.planeCount = dataThis.Walkers(i, 3);					% число орбитальных плоскостей в группе
-                group.f = dataThis.Walkers(i, 4);							% фазовый сдвиг по аргументу широты между КА в соседних плоскостях
-                group.altitude = dataThis.Walkers(i, 5);					% высота орбиты
-                group.maxRaan = deg2rad(dataThis.Walkers(i, 6));            % максимум прямого восхождения восходящего узла (при распределении орбитальных плоскостей)
-                group.startRaan = deg2rad(dataThis.Walkers(i, 7));			% прямое восхождение восходящего узла для первой плоскости
+            for groupIdx = 1:size(dataThis.Walkers, 1)
+                group.inclination = deg2rad(dataThis.Walkers(groupIdx, 1));        % наклонение орбитальной плоскости
+                group.satsPerPlane = dataThis.Walkers(groupIdx, 2);				   % число КА в каждой орбитальной плоскости группы
+                group.planeCount = dataThis.Walkers(groupIdx, 3);				   % число орбитальных плоскостей в группе
+                group.phaseShift = dataThis.Walkers(groupIdx, 4);				   % фазовый сдвиг по аргументу широты между КА в соседних плоскостях
+                group.altitude = dataThis.Walkers(groupIdx, 5);					   % высота орбиты
+                group.maxRaan = deg2rad(dataThis.Walkers(groupIdx, 6));            % максимум прямого восхождения восходящего узла (при распределении орбитальных плоскостей)
+                group.startRaan = deg2rad(dataThis.Walkers(groupIdx, 7));		   % прямое восхождение восходящего узла для первой плоскости
                 group.totalSatCount = group.satsPerPlane * group.planeCount;
 
-                this.groups{length(this.groups) + 1} = group;                
+                this.groupList{length(this.groupList) + 1} = group;                
                 this.totalSatCount = this.totalSatCount + group.totalSatCount;
             end
         end
@@ -57,36 +58,36 @@ classdef Constellation < handle
             this.state.elements = zeros(this.totalSatCount, 6);
             shift = 1;
 
-            for group = this.groups
+            for group = this.groupList
                 for i = 1:length(group{1})
                     ending = shift + group{1}(i).totalSatCount - 1;
                     this.state.elements(shift:ending,:) = this.getInitialElements(group{1});
                     shift = ending + 1;
-                end
-            end
-        end
+                end        % Конец цикла по спутникам внутри группы
+            end            % Конец цикла по группам
+        end                % Конец описания метода
 
         function elements = getInitialElements(this, group)
-            raans = linspace(group.startRaan, group.startRaan + group.maxRaan, group.planeCount + 1);
-            raans = mod(raans(1:end-1), 2 * pi);
+            raanList = linspace(group.startRaan, group.startRaan + group.maxRaan, group.planeCount + 1);
+            raanList = mod(raanList(1:end-1), 2 * pi);
 
             elements = zeros(group.totalSatCount, 6);
             idx = 1;
-            raanIDX = 0;
-            for raan = raans
-                for i = 0:group.satsPerPlane-1
+            raanIdx = 0;
+            for raan = raanList
+                for idxSat = 0:group.satsPerPlane-1 
                     sma = this.earthRadius + group.altitude * 1000;
-                    aol = 2 * pi / group.satsPerPlane * i + 2 * pi / group.totalSatCount * group.f * raanIDX;
+                    aol = 2 * pi / group.satsPerPlane * idxSat + 2 * pi / group.totalSatCount * group.phaseShift * raanIdx;
 
                     elements(idx, :) = [sma, 0, 0, raan, group.inclination, aol];
                     idx = idx + 1;
-                end
-                raanIDX = raanIDX + 1;
-            end
-        end        
+                end                    % Конец цикла по КА в орбитальной плоскости группы 
+                raanIdx = raanIdx + 1;
+            end                        % Конец цикла по орбитальным плоскостям 
+        end                            % Конец описания функции getInitialElements          
 
-        function propagateJ2(this, epochs)
-            this.state.eci = zeros(this.totalSatCount, 3, length(epochs));
+        function propagateJ2(this, epochList)
+            this.state.eci = zeros(this.totalSatCount, 3, length(epochList));
 
             sma         = this.state.elements(:, 1);
             inclination = this.state.elements(:, 5);            
@@ -96,14 +97,14 @@ classdef Constellation < handle
             raanPrecessionRate = -1.5 * (this.earthJ2 * this.earthGM^(1/2) * this.earthRadius^2) ./ (sma.^(7/2)) .* cos(inclination);
             draconicOmega      = sqrt(this.earthGM ./ sma.^3) .* (1 - 1.5 * this.earthJ2 .* (this.earthRadius ./ sma).^2) .* (1 - 4 .* cos(inclination).^2);
 
-            for epochIdx = 1:length(epochs)
-                aol = aol0 + epochs(epochIdx) * draconicOmega;
-                raanOmega = raan0 + epochs(epochIdx) * raanPrecessionRate;
+            for epochIdx = 1:length(epochList)
+                aol = aol0 + epochList(epochIdx) * draconicOmega;
+                raanOmega = raan0 + epochList(epochIdx) * raanPrecessionRate;
 
                 this.state.eci(:, :, epochIdx)  = [sma .* (cos(aol) .* cos(raanOmega) - sin(aol) .* cos(inclination) .* sin(raanOmega)), ...
                                                    sma .* (cos(aol) .* sin(raanOmega) + sin(aol) .* cos(inclination) .* cos(raanOmega)), ...
                                                    sma .* (sin(aol) .* sin(inclination))];
-            end
-        end
-    end
+            end    % Конец цикла по эпохам
+        end        % Конец функции propagateJ2
+    end            % Конец секции с описанием методов класса
 end
