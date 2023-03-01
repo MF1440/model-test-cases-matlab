fda24c76b4f6edd7ef0d7079c7df6c0a04d2fbd7
Ekaterina <katarinbgor@gmail.com>
2023-03-01 17:06:01 +0300

    Исправила ошибку; переписала код в читаемом  виде

--- MATLAB/solution.m
+++ MATLAB/solution.m
@@ -2,129 +2,144 @@ clc
 clear
 
 %% Исходные данные
-% диапазон для угла альфа (град.)
-alfaListDegrees = 2:2:70;
 
-% данные об абанентах
-load testDataFile.mat
+alfaListDegrees = 2:2:70;        % диапазон для угла альфа (град.)
 
+epoch = 1000;                    % произвольный момент времени
+utc = [2023 2 25 12 0 0]; 
+
+constellation_name = 'Starlink'; % Данные о спутниках
+
+load testDataFile.mat;           % Данные об абонентах
+
+%% Инициализация
 % Выделим абонентов с ненулевым запросом трафика
+users = getActiveUsers(coordsEcef, values);
+clear coordsEcef values
 
-activeUserIdx = find(values);
-activeUserList.Ecef = coordsEcef(activeUserIdx,:);      
-activeUserList.Value = values(activeUserIdx);
-activeUserList.originDist = sqrt( sum( (activeUserList.Ecef ).^2, 2 ));    % Удаленность абонентов от начала координат (можно было использовать R Земли, скорее всего)
-activeUserCount = length(activeUserIdx);
+% Выделим необходимые параметры группировки спутников
+sats = getSatStruct(constellation_name,epoch,utc);
 
-clear activeUserIdx coordsEcef values
+%%  Вычисляем логическую маску для абонентского спрса 
+mask = zeros(users.count, length(alfaListDegrees)); 
+alfaMatrixs = repmat(alfaListDegrees, users.count,1);
 
-% Спутники
-% создание объекта типа Constellation, инициализация параметрами группировки Starlink из конфига
-constellation = Constellation('Starlink');
-%вычисление элементов орбиты для всех КА в начальный момент
-constellation.getInitialState();
-% по условию момент времени произвольный
-epoch = 1000;
-% расчёт положений всех КА в заданный момент времени
-constellation.propagateJ2(epoch);
-% координаты спутников в инерциальной СК 
-satListEci = constellation.state.eci;
-
-%% преобразуем координаты спутников из eci в ecef
-
-satListEcef = zeros(size(satListEci));
-
-utc = [2023 2 25 12 0 0]; % произвольный момент времени
-
-tic
-parfor satIdx =  1:size(satListEci,1)
-    satListEcef(satIdx,:) = eci2ecef(utc, satListEci(satIdx,:));
+for groupIdx = 1:sats.groupCount        
+    groupMask = findGroupMask(users, sats, groupIdx, alfaMatrixs);  
+    mask = mask | groupMask;
 end
-toc
-
-%% Ищем для каждого абонента ближайший спутник на каждой из орбит
-
-tic
-[minDistUserSat, satIdxList] = findNearestSat(activeUserList, satListEcef, constellation);
-toc
-
-%% Найдем соотношение углов
-
-tic
-satOriginDist = sqrt( sum( (satListEcef ).^2, 2 ));             % Удаленность спутников от начала координат
-alfaMask = repmat(alfaListDegrees, activeUserCount,1);          % Альфа
-logicalMask = zeros(activeUserCount, length(alfaListDegrees));  % Логическая маска
-
-for groupIdx = 1:length(constellation.groupList)  
-    
-    a = satOriginDist(satIdxList(:,groupIdx));                  % Коротенькие переменные нужны были, чтобы не запутаться в формуле
-    b = minDistUserSat(:,groupIdx);
-    c = activeUserList.originDist;
-    
-    angleSatUser = acosd( (a.^2 + b.^2 - c.^2) ./ (2 .* a .* b) );
-    
-    temp = repmat(angleSatUser,1,length(alfaListDegrees));
-    tempMask = alfaMask > temp;
-    logicalMask = logicalMask | tempMask;
-    
-end
-toc
 
 %% Вычисляем трафик с учетом логической маски 
-
-ValueRep= repmat(activeUserList.Value, 1, length(alfaListDegrees));
-totalValue = sum(ValueRep .* logicalMask); 
+valuesMatrix= repmat(users.values, 1, length(alfaListDegrees));
+totalValue = sum(valuesMatrix .* mask); 
 
 %% Строим график
-    
-fig = figure;
-fig.Position = [500,300,900,400];
-plot(alfaListDegrees, totalValue, 'linewidth', 2); grid on
-xlabel('\alpha\circ', 'fontsize', 14)
-ylabel('TotalTraffic', 'fontsize', 14)
-title('Зависимость удовлетворённого спроса от угла \alpha\circ', 'fontsize', 14, 'FontWeight', 'normal')
+plotTotalValue(alfaListDegrees, totalValue)
 
-%% Функции 
+%% Функции
 
-function [minDistUserSat,satIdxList] = findNearestSat(activeUserList, satListEcef, constellation)
+function [users] = getActiveUsers(coordsEcef, values)
+    % Функция выделяет абонентов с ненулевым запросом трафика
     
-    % Расстояния от абонентов до ближайших спутников на каждой из орбит
-    minDistUserSat = zeros(length(activeUserList.Value), length(constellation.groupList));
-    
-    % Индексы ближайших спутников на каждой из орбит
-    satIdxList = zeros(length(activeUserList.Value),length(constellation.groupList));
-    
-    idx=1;
-    
-    for groupIdx = 1:length(constellation.groupList)
-    
-        totalSatCount = constellation.groupList{groupIdx}.totalSatCount;
-        satEqualAlt = satListEcef(idx : idx + totalSatCount -1, :);
-    
-        [minDistUserSat(:,groupIdx),satIdxList(:,groupIdx)] = findNearestInGroup(activeUserList.Ecef, satEqualAlt);                                                                                                                                       
-    
-        idx = idx + totalSatCount;
-    
-    end
-    
-    
+    activeUserIdx = find(values);
+    users.ecef = coordsEcef(activeUserIdx,:);            % Координаты активных абонентов    
+    users.values = values(activeUserIdx);                % Запрашиваемый трафик
+    users.module = sqrt( sum( (users.ecef ).^2, 2 ));    % Удаленность абонентов от начала координат (можно было использовать R Земли)
+    users.count = length(activeUserIdx);                 % Число активных абонентов
+
 end
 
-function[minDistUserSat,satInd] = findNearestInGroup(activeUserEcef, satListEcef)
+function [sats] = getSatStruct(constellation_name,epoch,utc)
+    % Функция инициалирует группировку спутников, пересчитывает координаты
+    % и возвращает структуру с необходимыми для дальнейших вычислений
+    % данными
     
-    distUserSat = zeros(size(activeUserEcef,1),size(satListEcef,1));
     
-    for sat = 1:size(satListEcef,1)   
-       satPos = repmat(satListEcef(sat,:),size(activeUserEcef,1),1);
-       distUserSat(:,sat) = sqrt( sum( (activeUserEcef - satPos).^2, 2 ));
+    % Инициализация группировки   
+    constellation = Constellation(constellation_name);
+    constellation.getInitialState();
+    constellation.propagateJ2(epoch);
+    satListEci = constellation.state.eci;
+
+    % преобразуем координаты спутников из eci в ecef
+    satListEcef = zeros(size(satListEci));
+
+    parfor satIdx =  1:size(satListEci,1)
+        satListEcef(satIdx,:) = eci2ecef(utc, satListEci(satIdx,:));
+    end
+
+    % Выделим группы спутников на разных высотах
+    idx = 1;
+    equalAltIdx = zeros(length(constellation.groupList),2);
+    for groupIdx = 1:length(constellation.groupList)
+        totalSatCount = constellation.groupList{groupIdx}.totalSatCount;
+        equalAltIdx(groupIdx, 1) = idx;
+        equalAltIdx(groupIdx, 2) = idx + totalSatCount -1;    
+        idx = idx + totalSatCount;
+    end
+    %
+    sats.ecef = satListEcef;
+    sats.module = sqrt( sum( (satListEcef ).^2, 2 )); % Удаленность спутников от начала координат
+    sats.equalAltIdx = equalAltIdx;
+    sats.groupCount = length(constellation.groupList);
+
+end
+
+function[distanceMin, nearestSat] = findNearestSat(users, sats, groupIdx)
+    %   Функция находит для всех абонентов ближайшие спутники в заданной
+    %   группе
+    %   distanceMin - расстояние абонент-ближайший спутник
+    %   nearestSat - индекс ближайшего спутника    
+    % Выделим спутники в текущей группе локальные переменные
+
+    firstIdx = sats.equalAltIdx(groupIdx,1);
+    lastIdx = sats.equalAltIdx(groupIdx,2);
+    
+    groupIdxRange = firstIdx:lastIdx;
+    satsCount = length(groupIdxRange);
+    
+    localSats.ecef = sats.ecef(groupIdxRange,:);
+    localSats.module = sats.module(groupIdxRange);
+    distance = zeros(users.count,satsCount);
+    
+    for sat = 1:satsCount   
+       satPos = repmat(localSats.ecef(sat,:),users.count,1);
+       distance(:,sat) = sqrt( sum( (users.ecef - satPos).^2, 2 ));
     end
     
     % расстоние до ближайшего спутника и индекс этого спутника для каждого абонента    
-    [minDistUserSat,satInd] = min(distUserSat,[],2);
+    [distanceMin, localIdx] = min(distance,[],2);
+    nearestSat = groupIdxRange(localIdx);
     
 end
 
+function [groupMask] = findGroupMask(users, sats, groupIdx, alfaMatrixs)
+    
+    % Функция проверяет, находится ли абонент в зоне радиовидимости
+    % ближайшего спутника из заданной группы.
+    % Возвращает логическую маску
+    
+    % Находим расстояние до ближайшего спутника и его индекс
+    [distance, nearestSat] = findNearestSat(users, sats, groupIdx);
+    
+    a = sats.module(nearestSat);% Коротенькие переменные нужны, чтобы не запутаться в формуле
+    b = distance;
+    c = users.module;
+    % Вычисляем угол
+    angleSatUser = acosd( (a.^2 + b.^2 - c.^2) ./ (2 .* a .* b) );
+    temp = repmat(angleSatUser,1,size(alfaMatrixs,2));
+    % Сравниваем вычисленный угол с углом alfa (по всей сетке)
+    groupMask = alfaMatrixs > temp;
+    
+end
 
+function [] = plotTotalValue(alfaListDegrees, totalValue)
 
-
-
+    fig = figure;
+    fig.Position = [500,300,900,400];
+    plot(alfaListDegrees, totalValue, 'linewidth', 2); grid on
+    xlabel('\alpha\circ', 'fontsize', 14)
+    ylabel('TotalTraffic', 'fontsize', 14)
+    title('Зависимость удовлетворённого спроса от угла \alpha\circ', 'fontsize', 14, 'FontWeight', 'normal')
+    
+end
