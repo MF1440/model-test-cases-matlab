a168c37b6a07677acfd658a2819a4d844a3a9c9a
SergeySuchkov <sergey.v.suchkov@gmail.com>
2023-09-04 00:32:55 +1000

    [suchkov] code review для тестового задания

---
+++ MATLAB/AstroConstants.m
@@ -0,0 +1,9 @@
+classdef AstroConstants
+
+   properties (Constant)
+    earthRadius = 6378135;           % Экваториальный радиус Земли [m]
+    earthGM = 3.986004415e+14;       % Гравитационный параметр Земли [m3/s2]
+    earthJ2 = 1.082626e-3;           % Вторая зональная гармоника геопотенциала
+   end
+   
+end
--- MATLAB/Constellation.m
+++ MATLAB/Constellation.m
@@ -1,14 +1,10 @@
 classdef Constellation < handle
+    % класс Constellation содержит в себе информацию обо всех КА во всех заданных группировках
 
     properties
         totalSatCount = 0;
         groups = {};
         state;
-
-        % константы
-        earthRadius = 6378135;           % Экваториальный радиус Земли [m]
-        earthGM = 3.986004415e+14;       % Гравитационный параметр Земли [m3/s2]
-        earthJ2 = 1.082626e-3;           % Вторая зональная гармоника геопотенциала
     end
 
     methods
@@ -20,32 +16,39 @@ classdef Constellation < handle
             this.loadFromConfigFile(varargin{1});
         end
 
-        function loadFromConfigFile(this, code)
-            fileName = 'constellationsTest.json';
+        function loadFromConfigFile(this, groupName)
+            % Загружает параметры группировки из json файла.
+            % Файл должен содержать в себе список группировок.
+            % Каждая группировака описывается структурой, в которой указано название группировки и
+            % кеплеровы элементы для каждого из эшелонов
+            % Пример:
+            %   [{"name": "Starlink","Walkers": [[53, 50, 32, 1, 500, 360, 0]]}]
+
+            fileName = ['..' filesep 'constellationsTest.json'];
             str = fileread(fileName);
             data = jsondecode(str);
             dataThis = [];
 
             for i = 1:length(data)
-                if strcmpi(data(i).name, code)
+                if strcmpi(data(i).name, groupName)
                     dataThis = data(i);
                     break
                 end
             end
 
             if isempty(dataThis)
-                disp('Группировка не найдена в файле');
+                disp(['Группировка ' groupName ' не найдена в файле']);
                 return
             end
 
             for i = 1:size(dataThis.Walkers, 1)
-                group.inclination = deg2rad(dataThis.Walkers(i, 1));        % наклонение орбитальной плоскости
+                group.inclination = deg2rad(dataThis.Walkers(i, 1));        % наклонение орбитальной плоскости в градусах
                 group.satsPerPlane = dataThis.Walkers(i, 2);				% число КА в каждой орбитальной плоскости группы
                 group.planeCount = dataThis.Walkers(i, 3);					% число орбитальных плоскостей в группе
-                group.f = dataThis.Walkers(i, 4);							% фазовый сдвиг по аргументу широты между КА в соседних плоскостях
-                group.altitude = dataThis.Walkers(i, 5);					% высота орбиты
-                group.maxRaan = deg2rad(dataThis.Walkers(i, 6));            % максимум прямого восхождения восходящего узла (при распределении орбитальных плоскостей)
-                group.startRaan = deg2rad(dataThis.Walkers(i, 7));			% прямое восхождение восходящего узла для первой плоскости
+                group.phaseShift = dataThis.Walkers(i, 4);				    % фазовый сдвиг по аргументу широты между КА в соседних плоскостях. Варьируется от 0 до количества орбит в группе
+                group.altitude = dataThis.Walkers(i, 5);					% высота орбиты в км
+                group.maxRaan = deg2rad(dataThis.Walkers(i, 6));            % максимум прямого восхождения восходящего узла (при распределении орбитальных плоскостей) в градусах
+                group.startRaan = deg2rad(dataThis.Walkers(i, 7));			% прямое восхождение восходящего узла для первой плоскости в градусах
                 group.totalSatCount = group.satsPerPlane * group.planeCount;
 
                 this.groups{length(this.groups) + 1} = group;                
@@ -54,6 +57,8 @@ classdef Constellation < handle
         end
 
         function getInitialState(this)
+            % Записывает массив кеплеровых элементов орбит для всех эшелонов
+
             this.state.elements = zeros(this.totalSatCount, 6);
             shift = 1;
 
@@ -67,16 +72,18 @@ classdef Constellation < handle
         end
 
         function elements = getInitialElements(this, group)
-            raans = linspace(group.startRaan, group.startRaan + group.maxRaan, group.planeCount + 1);
-            raans = mod(raans(1:end-1), 2 * pi);
+            % Расчитывает кеплеровы элементы орбит для каждого эшелона
+
+            raanArray = linspace(group.startRaan, group.startRaan + group.maxRaan, group.planeCount + 1);
+            raanArray = mod(raanArray(1:end-1), 2 * pi);
 
             elements = zeros(group.totalSatCount, 6);
             idx = 1;
             raanIDX = 0;
-            for raan = raans
+            for raan = raanArray
                 for i = 0:group.satsPerPlane-1
-                    sma = this.earthRadius + group.altitude * 1000;
-                    aol = 2 * pi / group.satsPerPlane * i + 2 * pi / group.totalSatCount * group.f * raanIDX;
+                    sma = AstroConstants.earthRadius + group.altitude * 1000;
+                    aol = 2 * pi / group.satsPerPlane * i + 2 * pi / group.totalSatCount * group.phaseShift * raanIDX;
 
                     elements(idx, :) = [sma, 0, 0, raan, group.inclination, aol];
                     idx = idx + 1;
@@ -85,20 +92,25 @@ classdef Constellation < handle
             end
         end        
 
-        function propagateJ2(this, epochs)
-            this.state.eci = zeros(this.totalSatCount, 3, length(epochs));
+        function propagateJ2(this, epochArray)
+            % Расчитывает eci координаты всех КА в указанные эпохи
+
+            this.state.eci = zeros(this.totalSatCount, 3, length(epochArray));
 
             sma         = this.state.elements(:, 1);
             inclination = this.state.elements(:, 5);            
             raan0       = this.state.elements(:, 4);
             aol0        = this.state.elements(:, 6);
 
-            raanPrecessionRate = -1.5 * (this.earthJ2 * this.earthGM^(1/2) * this.earthRadius^2) ./ (sma.^(7/2)) .* cos(inclination);
-            draconicOmega      = sqrt(this.earthGM ./ sma.^3) .* (1 - 1.5 * this.earthJ2 .* (this.earthRadius ./ sma).^2) .* (1 - 4 .* cos(inclination).^2);
+            raanPrecessionRate = -1.5 * (AstroConstants.earthJ2 * AstroConstants.earthGM^(1/2) * ...
+                                    AstroConstants.earthRadius^2) ./ (sma.^(7/2)) .* cos(inclination);
+            draconicOmega      = sqrt(AstroConstants.earthGM ./ sma.^3) .* ...
+                                    (1 - 1.5 * AstroConstants.earthJ2 .* (AstroConstants.earthRadius ./ sma).^2) .* ...
+                                    (1 - 4 .* cos(inclination).^2);
 
-            for epochIdx = 1:length(epochs)
-                aol = aol0 + epochs(epochIdx) * draconicOmega;
-                raanOmega = raan0 + epochs(epochIdx) * raanPrecessionRate;
+            for epochIdx = 1:length(epochArray)
+                aol = aol0 + epochArray(epochIdx) * draconicOmega;
+                raanOmega = raan0 + epochArray(epochIdx) * raanPrecessionRate;
 
                 this.state.eci(:, :, epochIdx)  = [sma .* (cos(aol) .* cos(raanOmega) - sin(aol) .* cos(inclination) .* sin(raanOmega)), ...
                                                    sma .* (cos(aol) .* sin(raanOmega) + sin(aol) .* cos(inclination) .* cos(raanOmega)), ...
